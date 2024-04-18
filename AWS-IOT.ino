8b0c4fc76685af81dccaf2764969d3991a43a92c
Cydnirn <seno@siotics.org>
2024-04-18 22:00:19 +0700

    feat: Add random number generator

--- AWS-IOT.ino
+++ AWS-IOT.ino
@@ -3,81 +3,85 @@
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
 #include "WiFi.h"
- 
-#define AWS_IOT_PUBLISH_TOPIC   "esp32/team/temp"
 
- 
+#define AWS_IOT_PUBLISH_TOPIC "esp32/team/temp"
+
 WiFiClientSecure net = WiFiClientSecure();
 PubSubClient client(net);
- 
+
+int lb = 0, ub = 50;
+
 void connectAWS()
 {
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
- 
+
   Serial.println("Connecting to Wi-Fi");
- 
+
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
     Serial.print(".");
   }
- 
+
   // Configure WiFiClientSecure to use the AWS IoT device credentials
   net.setCACert(AWS_CERT_CA);
   net.setCertificate(AWS_CERT_CRT);
   net.setPrivateKey(AWS_CERT_PRIVATE);
- 
+
   // Connect to the MQTT broker on the AWS endpoint we defined earlier
   client.setServer(AWS_IOT_ENDPOINT, 8883);
- 
+
   // Create a message handler
   client.setCallback(messageHandler);
- 
+
   Serial.println("Connecting to AWS IOT");
- 
+
   while (!client.connect(THINGNAME))
   {
     Serial.print(".");
     delay(100);
   }
- 
+
   if (!client.connected())
   {
     Serial.println("AWS IoT Timeout!");
     return;
   }
- 
+
   Serial.println("AWS IoT Connected!");
 }
- 
+
 void publishMessage()
 {
   StaticJsonDocument<200> doc;
-  doc["message"] = "Hello, World!";
+
+  int temp = random(lb, ub);
+  doc["temp"] = temp;
   char jsonBuffer[512];
   serializeJson(doc, jsonBuffer); // print to client
- 
+
   client.publish(AWS_IOT_PUBLISH_TOPIC, jsonBuffer);
 }
- 
-void messageHandler(char* topic, byte* payload, unsigned int length)
+
+void messageHandler(char *topic, byte *payload, unsigned int length)
 {
   Serial.print("incoming: ");
   Serial.println(topic);
- 
+
   StaticJsonDocument<200> doc;
   deserializeJson(doc, payload);
-  const char* message = doc["message"];
+  const char *message = doc["message"];
   Serial.println(message);
 }
- 
+
 void setup()
 {
   Serial.begin(115200);
+  randomSeed(analogRead(0));
   connectAWS();
 }
- 
+
 void loop()
 {
   publishMessage();
